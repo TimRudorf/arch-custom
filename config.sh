05c22c6cc599acf63b3d0df364d178f6a03408d3
Tim Rudorf <tim.rudorf@icloud.com>
2025-11-29 16:25:33 +0100

    config script

---
+++ config.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# CONSTANTS
+declare -r DIR_USER_HOME=$(eval echo ~$USER)
+declare -r DIR_USER_CONFIG=$DIR_USER_HOME/.config
+declare -r DIR_SCRIPT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# default option values
+OPTION_DEBUG=false
+
+# check if debug is enabled
+for parameter in "$@"
+do
+  case $parameter in
+
+    -d | --debug)
+      printf 'debug has been enabled\n'
+      OPTION_DEBUG=true
+      ;;
+
+    *)
+      printf "option '$parameter' is unknown\n"
+      exit 1
+      ;;
+  esac
+done
+
+# ask for sudo permission
+sudo -v
+if [[ "$(sudo id -u)" -ne 0 ]]; then
+  printf 'This script must be run with sudo\n'
+fi
+
+# change starship config to catppuccin
+starship preset catppuccin-powerline -o ~/.config/starship.toml
+
+# change zellij config to catppuccin
+mkdir -p $DIR_USER_CONFIG/zellij/themes
+curl https://raw.githubusercontent.com/catppuccin/zellij/refs/heads/main/catppuccin.kdl > $DIR_USER_CONFIG/zellij/themes/catppuccin.kdl
+zellij options --theme catppuccin
+
+# stow all config files
+stow --adopt --dir $DIR_SCRIPT/dotfiles --target $DIR_USER_HOME *
+git restore .
